28a3b1e216e6bc96e3f301e39b534e9fc7b6fe69
Addis Gashaw Zegeye <AddisGashaw@cbe.com.et>
2025-08-06 18:28:04 +0300

    second commit

--- src/Pages/PlantDetail.jsx
+++ src/Pages/PlantDetail.jsx
@@ -98,12 +98,12 @@ const handleSubImageClick = (clickedImg) => {
           <svg className={`w-4 h-4 mr-1`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
           </svg>
-          {t('breadcrumbs.home')}
+          {t('plantDetail.home')}
         </Link>
         <IoIosArrowForward className={`mx-2 text-xs text-gray-400 `} />
         <Link to="/shop" className="hover:text-green-600 transition-colors flex items-center">
           <FaLeaf className={`w-3 h-3 mr-1`} />
-          {t('breadcrumbs.plants')}
+          {t('plantDetail.plant')}
         </Link>
         <IoIosArrowForward className={`mx-2 text-xs text-gray-400 `} />
         <span className="text-green-600 font-medium">{getPlantText(plant, 'names')}</span>
